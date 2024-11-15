f4df94006703cc13b423c0dbf0a9b8dccd1fa967
Nerim <nerim5037@gmail.com>
2024-11-15 22:49:52 +0900

    chore: 매개변수명 변경

--- signup/js/index.js
+++ signup/js/index.js
@@ -3,8 +3,8 @@ import { userInform, actionSubmit, pswrViewHide, btnVisible, inputList } from ".
 // 로그인/회원가입
 // input 입력 조건에 따른 UI 변경과 제출버튼 활성화/비활성화
 for(let i of inputList){
-    i.addEventListener('keyup', (id) => {
-        userInform(id);
+    i.addEventListener('keyup', (item) => {
+        userInform(item);
         actionSubmit();
     });
 };
