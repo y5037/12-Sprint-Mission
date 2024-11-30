3b75e79fc4f13dd2227d3f2007927fada5dae3f9
Nerim <nerim5037@gmail.com>
2024-11-30 22:47:46 +0900

    feat: Javascript로 제작했던 로그인 페이지를 리액트 버전으로 추가

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,11 +1,90 @@
+import { Link } from "react-router-dom";
+import EmailValidation from "../App/EmailValidation";
 import styles from "../../Styles/Login/Login.module.css";
 import LogoImg from "../../Assets/images/login/login_logo.png";
 import GoogleImg from "../../Assets/images/login/google.svg";
 import KaKaoImg from "../../Assets/images/login/kakao.svg";
 import InVisibleImg from "../../Assets/images/login/btn_invisible.svg";
-import { Link } from "react-router-dom";
+import VisibleImg from "../../Assets/images/login/btn_visible.svg";
+import { useEffect, useState } from "react";
 
 function LoginForm() {
+  const [getId, setGetId] = useState("");
+  const [isEmail, setIsEmail] = useState("");
+  const [isPassword, setIsPassword] = useState("");
+  const [emailWarnMsg, setEmailWarnMsg] = useState("");
+  const [passwordWarnMsg, setPasswordWarnMsg] = useState("");
+  const [emailRequiredChk, setEmailRequiredChk] = useState(true);
+  const [passwordRequiredChk, setPasswordRequiredChk] = useState(true);
+  const [emailErrorChk, setEmailErrorChk] = useState(false);
+  const [passwordErrorChk, setPasswordErrorChk] = useState(false);
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [isSubmit, setIsSubmit] = useState("");
+
+  const getUserEmailInfo = (e) => {
+    setGetId(e.target.id);
+    setIsEmail(e.target.value);
+  };
+
+  const getUserPasswordInfo = (e) => {
+    setGetId(e.target.id);
+    setIsPassword(e.target.value);
+  };
+
+  const emailErrorContext = (txt, flag) => {
+    setEmailWarnMsg(txt);
+    setEmailRequiredChk(flag);
+  };
+
+  const passwordErrorContext = (txt, flag) => {
+    setPasswordWarnMsg(txt);
+    setPasswordRequiredChk(flag);
+  };
+
+  const handlePasswordVisible = (e) => {
+    if (passwordVisible) {
+      setPasswordVisible(false);
+    } else {
+      setPasswordVisible(true);
+    }
+  };
+
+  useEffect(() => {
+    switch (getId) {
+      case "useremail":
+        if (isEmail === "") {
+          emailErrorContext("이메일을 입력해주세요", true);
+          setEmailErrorChk(true);
+        } else if (isEmail !== "" && !EmailValidation(isEmail)) {
+          emailErrorContext("잘못된 이메일 형식입니다", true);
+          setEmailErrorChk(true);
+        } else if (EmailValidation(isEmail)) {
+          emailErrorContext("", false);
+          setEmailErrorChk(false);
+        }
+        break;
+      case "userpw":
+        if (isPassword === "") {
+          passwordErrorContext("비밀번호를 입력해주세요", true);
+          setPasswordErrorChk(true);
+        } else if (isPassword.length < 8) {
+          passwordErrorContext("비밀번호를 8자 이상 입력해주세요", true);
+          setPasswordErrorChk(true);
+        } else if (isPassword.length >= 8) {
+          passwordErrorContext("", false);
+          setPasswordErrorChk(false);
+        }
+    }
+  }, [isEmail, isPassword]);
+
+  useEffect(() => {
+    if (emailRequiredChk !== true && passwordRequiredChk !== true) {
+      setIsSubmit(true);
+    } else {
+      setIsSubmit(false);
+    }
+  }, [emailRequiredChk, passwordRequiredChk]);
+
   return (
     <>
       <div className={styles.containWrap}>
@@ -23,12 +102,15 @@ function LoginForm() {
                 <div className={styles.cover}>
                   <input
                     id="useremail"
+                    className={`${emailErrorChk ? styles.inputError : ""} ${
+                      styles.input
+                    }`}
                     type="email"
                     placeholder="이메일을 입력해주세요"
-                    className={styles.input}
-                    required
+                    onChange={getUserEmailInfo}
+                    required={emailRequiredChk ? true : false}
                   />
-                  <p className={styles.txtWarning}></p>
+                  <p className={styles.txtWarning}>{emailWarnMsg}</p>
                 </div>
               </div>
               <div className={`${styles.inputBox} ${styles.pointer}`}>
@@ -36,19 +118,33 @@ function LoginForm() {
                 <div className={styles.cover}>
                   <input
                     id="userpw"
-                    type="password"
+                    className={`${passwordErrorChk ? styles.inputError : ""} ${
+                      styles.input
+                    }`}
+                    type={passwordVisible ? "text" : "password"}
                     placeholder="비밀번호를 입력해주세요"
-                    className={styles.input}
-                    required
+                    onChange={getUserPasswordInfo}
+                    required={passwordRequiredChk ? true : false}
                   />
-                  <p className={styles.txtWarning}></p>
-                  <button type="button" className={styles.btnVisible}>
-                    <img src={InVisibleImg} alt="비밀번호 보기" />
+                  <p className={styles.txtWarning}>{passwordWarnMsg}</p>
+                  <button
+                    type="button"
+                    className={styles.btnVisible}
+                    onClick={handlePasswordVisible}
+                  >
+                    <img
+                      src={passwordVisible ? VisibleImg : InVisibleImg}
+                      alt="비밀번호 보기"
+                    />
                   </button>
                 </div>
               </div>
               <div className={styles.btnBox}>
-                <button type="button" className={styles.btnSubmit} disabled>
+                <button
+                  type="button"
+                  className={styles.btnSubmit}
+                  disabled={isSubmit ? false : true}
+                >
                   로그인
                 </button>
               </div>
