a9c496f2dc1de279dc2a1d7dbe07ff9d1ee3c39c
Nerim <nerim5037@gmail.com>
2024-12-05 15:31:04 +0900

    refactor(mentor): 코드를 직관적으로 볼 수 있도록 컴포넌트로 분리

--- src/Components/ProductList/GeneralItemsList.js
+++ src/Components/ProductList/GeneralItemsList.js
@@ -4,6 +4,15 @@ import notFoundImg from "../../assets/images/productList/not_found.png";
 import GeneralSearchForm from "./GeneralSearchForm";
 import GeneralItem from "./GeneralItem";
 
+function EmptyPlaceholder() {
+  return (
+    <div className={styles.emptyList}>
+      <img src={notFoundImg} alt="Not Found" />
+      <p>검색하신 상품을 찾을 수 없습니다</p>
+    </div>
+  );
+}
+
 function GeneralItemsList({
   page,
   setPage,
@@ -23,20 +32,15 @@ function GeneralItemsList({
         setIsDataCount={setIsDataCount}
       />
       <ul className={styles.productCover}>
-        {productContainer.length !== 0 ? (
+        {productContainer.length === 0 && <EmptyPlaceholder />}
+        {productContainer.length > 0 &&
           productContainer.map((item) => {
             return (
               <li key={item.id} className={styles.item}>
                 <GeneralItem item={item} />
               </li>
             );
-          })
-        ) : (
-          <div className={styles.emptyList}>
-            <img src={notFoundImg} alt="Not Found" />
-            <p>검색하신 상품을 찾을 수 없습니다</p>
-          </div>
-        )}
+          })}
       </ul>
     </div>
   );
