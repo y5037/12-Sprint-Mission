d72ddd024dcacfe6f967db7d23e2ee7b4773a096
Nerim <nerim5037@gmail.com>
2025-01-09 17:43:43 +0900

    chore: 상품 디테일 설명란 CSS 수정

---
+++ dist/assets/App-C6xAiKq_.js
@@ -0,0 +1,1 @@
+import{j as t,O as r}from"./index-Csnxd7_5.js";function e(){return t.jsx(r,{})}export{e as default};
--- dist/assets/App-DtvwKNUc.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as r}from"./index-WDyK7aTB.js";function e(){return t.jsx(r,{})}export{e as default};
